cb5f81af8d6677f4c5c98f69a2c110d731304856
Archit3009 <guptaarchit615@gmail.com>
2026-06-14 16:38:05 +0530

    Autowired Annotation

---
+++ src/main/java/com/example/autowired/annotation/Employee.java
@@ -0,0 +1,58 @@
+package com.example.autowired.annotation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("employee")
+public class Employee {
+   @Value("695")
+    private int empid;
+  @Value("Archit")
+   private String firstname;
+  @Value("${java.home}") // we have been adding a property here instead of writing the value ourselves
+  private String lastname;
+  @Value("#{200000*12}") // inserting an expression here and the final answer will be stored
+  private double salary;
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public int getEmpid() {
+        return empid;
+    }
+
+    public void setEmpid(int empid) {
+        this.empid = empid;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empid=" + empid +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
